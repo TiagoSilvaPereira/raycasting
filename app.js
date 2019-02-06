f662ef1391e139116764ad85f0da57fcbf53a75f
Tiago Rodrigues <contato@kingofcode.com.br>
2019-02-06 11:47:21 -0200

    Moving the player and updating on minimap

--- app.js
+++ app.js
@@ -7,10 +7,59 @@ var app = {
         this.mapHeight = this.map.length;		// Number of map blocks in y-direction
         this.miniMapScale = 8;	                // How many pixels to draw a map block
 
+        this.setupInputs();
+        this.setupPlayer();
         this.drawMiniMap();
+        this.updateScene();
+    },
+
+    setupInputs() {
+        
+        document.onkeydown = function(e) {
+            switch (event.keyCode) {
+                // Up, move player forward, ie. increase speed
+                case 38:
+                    this.player.speed = 1; break;
+                // Down, move player backward, set negative speed
+                case 40:
+                    this.player.speed = -1; break;
+                // Left, rotate player left
+                case 37:
+                    this.player.direction = -1; break;
+                // Right, rotate player right
+                case 39:
+                    this.player.direction = 1; break;
+            }
+        }.bind(this);
+
+        // Stop the player movement/rotation when the keys are released
+        document.onkeyup = function(event) {
+            switch (event.keyCode) {
+                case 38:
+                case 40:
+                    this.player.speed = 0; break;
+                case 37:
+                case 39:
+                    this.player.direction = 0; break;
+            }
+        }.bind(this);
+
+    },
+
+    setupPlayer() {
+        this.player = {
+            x: 16,
+            y: 10,
+            direction: 0,
+            rotation: 0,
+            speed: 0,
+            moveSpeed: 0.18,
+            rotationSpeed: 6 * Math.PI / 180
+        };
     },
 
     drawMiniMap() {
+        
         let miniMap = document.getElementById('minimap');
 
         miniMap.width = this.mapWidth * this.miniMapScale;
@@ -37,9 +86,57 @@ var app = {
 
     },
 
+    movePlayer() {
+        let moveStep = this.player.speed * this.player.moveSpeed;
+        
+        this.player.rotation += this.player.direction * this.player.rotationSpeed;
+
+        //console.log(this.player.rotation)
+
+        // Calculate player position using sine/cosine of player rotation
+        this.player.x = this.player.x + Math.cos(this.player.rotation) * moveStep;
+        this.player.y = this.player.y + Math.sin(this.player.rotation) * moveStep;
+
+        //console.log(this.player.x, this.player.y);
+    },
+
+    updateMiniMap() {
+        var miniMap = document.getElementById('minimap');
+        var miniMapObjects = document.getElementById('minimapobjects');
+
+        var objectContext = miniMapObjects.getContext('2d');
+        miniMapObjects.width = miniMapObjects.width;
+
+        objectContext.fillStyle = 'red';
+        objectContext.fillRect(		// Draw a dot at the current player position
+            this.player.x * this.miniMapScale - 2, 
+            this.player.y * this.miniMapScale - 2,
+            4, 4
+        );
+
+        objectContext.strokeStyle = 'red';
+        objectContext.beginPath();
+        objectContext.moveTo(this.player.x * this.miniMapScale, this.player.y * this.miniMapScale);
+        
+        objectContext.lineTo(
+            (this.player.x + Math.cos(this.player.rotation) * 4) * this.miniMapScale,
+            (this.player.y + Math.sin(this.player.rotation) * 4) * this.miniMapScale
+        );
+
+        objectContext.closePath();
+        objectContext.stroke();
+    },
+
     drawScreen() {
 
-    }
+    },
+
+    updateScene() {
+        this.movePlayer();
+        this.updateMiniMap();
+
+        setTimeout(this.updateScene.bind(this), 1000/30); // 30 FPS
+    },
 
 }
 
